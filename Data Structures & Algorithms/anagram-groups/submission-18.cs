d443f37c5b48797dbc9bacdef771e8ad1c733c21
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-06-08 12:17:50 +0500

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.cs
@@ -0,0 +1,21 @@
+public class Solution 
+{
+    public List<List<string>> GroupAnagrams(string[] strs) 
+    {
+        var result = new Dictionary<string, List<string>>();
+
+        foreach (var str in strs)
+        {
+            var chars = str.ToCharArray();
+            Array.Sort(chars);
+            var key = new string(chars);
+
+            if (!result.ContainsKey(key))
+                result[key] = new List<string>();
+
+            result[key].Add(str);
+        }
+
+        return result.Values.ToList();
+    }
+}
