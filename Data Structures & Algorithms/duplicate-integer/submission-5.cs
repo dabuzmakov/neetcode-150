ad80992ae8e64eb14d615aac5941ede91c437773
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-06-08 12:16:44 +0500

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) 
+    {
+        var setNums = new HashSet<int>();
+        
+        foreach (var num in nums)
+            if (!setNums.Add(num))
+                return true;
+
+        return false;
+    }
+}
