e46ec576126f44023118ab6b71e6821abaac5e2f
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-06-11 13:37:45 +0500

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public int CarFleet(int target, int[] position, int[] speed)
+    {
+        var cars = position
+            .Zip(speed, (s, v) => (s, v, t: (double)(target - s) / v))
+            .OrderByDescending(car => car.s);
+        
+        var stack = new Stack<(int s, int v, double t)>();
+        
+        foreach (var car in cars)
+            if (stack.Count == 0 || stack.Peek().t < car.t)
+                stack.Push(car);
+        
+        return stack.Count();
+    }
+}
