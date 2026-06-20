7bcea3df7a6b7f29a679e883c419b8258db8de8b
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-06-20 11:19:47 +0500

    Add: linked-list-cycle-detection - submission-5

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-5.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+
+        while (fast != null)
+        {
+            fast = fast?.next?.next;
+            
+            if (fast == slow)
+                return true;
+
+            slow = slow?.next;
+        }
+
+        return false;
+    }
+}
