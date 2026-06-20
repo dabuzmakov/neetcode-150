5cf6b2af54a173fa584ba5d2ed6bcdfacf7a94a3
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-06-20 14:18:16 +0500

    Add: reorder-linked-list - submission-14

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-14.cs
@@ -0,0 +1,48 @@
+public class Solution
+{
+    public void ReorderList(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        var second = slow.next;
+        slow.next = null;
+        
+        second = Reverse(second);
+
+        var current = head;
+        while (current != null && second != null)
+        {
+            var nextFirst = current.next;
+            var nextSecond = second.next;
+
+            current.next = second;
+            second.next = nextFirst;
+
+            current = nextFirst;
+            second = nextSecond;
+        }
+    }
+
+    public ListNode Reverse(ListNode head)
+    {
+        ListNode previous = null;
+        var current = head;
+
+        while (current != null)
+        {
+            var nextNode = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextNode;
+        }
+
+        return previous;
+    }
+}
