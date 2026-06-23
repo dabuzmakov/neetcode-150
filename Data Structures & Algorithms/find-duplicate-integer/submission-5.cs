469ef6f6327edab7ff86bfda9cf6a4e2d548e178
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-06-23 12:35:31 +0500

    Add: find-duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-5.cs
@@ -0,0 +1,14 @@
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var abs = Math.Abs(nums[i]);
+            if (nums[abs] < 0) return abs; 
+            nums[abs] *= -1;
+        }
+
+        return 0;
+    }
+}
