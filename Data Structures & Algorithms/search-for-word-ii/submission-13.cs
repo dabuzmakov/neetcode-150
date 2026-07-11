2637118e4b99d25c601944e12883598055fd4b60
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-07-11 15:57:53 +0500

    Add: search-for-word-ii - submission-13

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-13.cs
@@ -0,0 +1,78 @@
+public class Solution
+{
+    private char[][] _board;
+    private string[] _words;
+    private List<string> _result;
+
+    private readonly (int, int)[] _offset
+        = { (-1, 0), (1, 0), (0, -1), (0, 1), };
+
+    public List<string> FindWords(char[][] board, string[] words)
+    {
+        (_board, _words, _result) = (board, words, new());
+
+        var trie = new PrefixTree();
+        for (var i = 0; i < words.Length; i++)
+            trie.Add(words[i], i);
+
+        for (var i = 0; i < board.Length; i++)
+        for (var j = 0; j < board[0].Length; j++)
+            Search(i, j, trie.Root);
+        
+        return _result;
+    }
+
+    public void Search(int i, int j, TrieNode curNode)
+    {
+        var sym = _board[i][j];
+        if (sym == '*' || !curNode.Children.TryGetValue(_board[i][j], out var newNode))
+            return;
+        
+        _board[i][j] = '*';
+
+        if (newNode.Index != -1)
+        {
+            _result.Add(_words[newNode.Index]);
+            newNode.Index = -1;
+        }
+
+        foreach (var (dx, dy) in _offset)
+            if (InBounds(i + dx, j + dy))
+                Search(i + dx, j + dy, newNode);
+
+        _board[i][j] = sym;
+    }
+
+    public bool InBounds(int x, int y)
+        => (x >= 0) && (x < _board.Length)
+        && (y >= 0) && (y < _board[0].Length);
+
+    public class PrefixTree
+    {
+        public TrieNode Root { get; private set; } = new();
+        
+        public void Add(string word, int index)
+        {
+            var current = Root;
+
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (!current.Children.TryGetValue(word[i], out var node))
+                {
+                    node = new TrieNode();
+                    current.Children[word[i]] = node;
+                }
+
+                current = node;
+            }
+
+            current.Index = index;
+        }
+    }
+
+    public class TrieNode
+    {
+        public Dictionary<char, TrieNode> Children { get; } = new();
+        public int Index { get; set; } = -1;
+    }
+}
