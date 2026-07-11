0be76862cbb7fc778e87aa4b405a11e33e3861b4
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-07-11 13:20:26 +0500

    Add: design-word-search-data-structure - submission-12

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-12.cs
@@ -0,0 +1,58 @@
+public class WordDictionary
+{
+    public class TrieNode
+    {
+        public bool IsKey { get; set; }
+        public Dictionary<char, TrieNode> Children { get; set; }
+
+        public TrieNode(bool isKey = false)
+        {
+            IsKey = isKey;
+            Children = new Dictionary<char, TrieNode>();
+        }
+    }
+
+    public TrieNode Root { get; set; }
+
+    public WordDictionary()
+        => Root = new TrieNode();
+    
+    public void AddWord(string word)
+    {
+        var current = Root;
+
+        for (var i = 0; i < word.Length; i++)
+        {
+            if (!current.Children.TryGetValue(word[i], out var node))
+            {
+                node = new TrieNode();
+                current.Children[word[i]] = node;
+            }
+
+            current = node;
+        }
+
+        current.IsKey = true;
+    }
+    
+    public bool Search(string word)
+        => Search(Root, word, 0);
+
+    private bool Search(TrieNode curNode, string word, int index)
+    {
+        while (index < word.Length)
+        {
+            if (word[index] == '.')
+                foreach (var child in curNode.Children.Values)
+                    if (Search(child, word, index + 1))
+                        return true;
+
+            if (!curNode.Children.TryGetValue(word[index++], out var node))
+                return false;
+            
+            curNode = node;
+        }
+
+        return curNode.IsKey;
+    }
+}
