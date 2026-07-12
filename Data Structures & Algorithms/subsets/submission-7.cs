484be62c2aaa44722cf3cebb4f6edd92831ad499
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-07-12 14:30:28 +0500

    Add: subsets - submission-7

---
+++ Data Structures & Algorithms/subsets/submission-7.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public List<List<int>> Subsets(int[] nums)
+    {
+        var result = new List<List<int>>();
+        result.Add(new List<int>());
+
+        foreach (var num in nums)
+        {
+            var size = result.Count;
+
+            for (var i = 0; i < size; i++)
+            {
+                var newList = new List<int>(result[i]);
+                newList.Add(num);
+                result.Add(newList);
+            }
+        }
+
+        return result;
+    }
+}
