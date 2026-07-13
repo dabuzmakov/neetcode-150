41bc9b14c0d36900c61cbff05c79be203549c622
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-07-13 09:29:52 +0500

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.cs
@@ -0,0 +1,32 @@
+public class Solution
+{
+    private List<List<int>> _result = new();
+    private int[] _nums;
+
+    public List<List<int>> Permute(int[] nums) 
+    {
+        _nums = nums;
+        FindAll(new List<int>(), new HashSet<int>());
+        return _result;
+    }
+
+    private void FindAll(List<int> current, HashSet<int> used)
+    {
+        if (current.Count == _nums.Length)
+        {
+            _result.Add(new List<int>(current));
+            return;
+        }
+
+        foreach (var num in _nums)
+        {
+            if (used.Contains(num)) continue;
+            used.Add(num);
+
+            current.Add(num);
+            FindAll(current, used);
+            current.RemoveAt(current.Count - 1);
+            used.Remove(num);
+        }
+    }
+}
