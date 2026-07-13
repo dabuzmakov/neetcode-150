3b770883cae8ee50a3cce2bffb763e5cc8e854b4
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-07-13 09:32:16 +0500

    Add: combination-target-sum - submission-12

---
+++ Data Structures & Algorithms/combination-target-sum/submission-12.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    private int[] _nums;
+    private int _target;
+    private List<List<int>> _result = new();
+
+    public List<List<int>> CombinationSum(int[] nums, int target)
+    {
+        (_target, _nums) = (target, nums);
+        FindAll(new List<int>(), 0, 0);
+        return _result;
+    }
+
+    private void FindAll(List<int> currentNums, int currentSum, int start)
+    {
+        if (currentSum > _target) return;
+
+        if (currentSum == _target)
+        {
+            _result.Add(new List<int>(currentNums));
+            return;
+        }
+        
+        for (var i = start; i < _nums.Length; i++)
+        {
+            currentNums.Add(_nums[i]);
+            FindAll(currentNums, currentSum + _nums[i], i);
+            currentNums.RemoveAt(currentNums.Count - 1);
+        }
+    }
+}
