72033e96bf6c75b21d9837c2492a722f865046dd
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-08-09 13:00:15 +0500

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost) 
+    {
+        var dpTable = new int[cost.Length];
+        (dpTable[0], dpTable[1]) = (cost[0], cost[1]);
+        
+        for (var i = 2; i < cost.Length; i++)
+            dpTable[i] = Math.Min(dpTable[i - 1], dpTable[i - 2]) + cost[i];
+
+        return Math.Min(dpTable[cost.Length - 1], dpTable[cost.Length - 2]);
+    }
+}
