58e37f98c5b3e30019821f8ee46c109d1a251131
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-08-11 10:57:48 +0500

    Add: coin-change - submission-16

---
+++ Data Structures & Algorithms/coin-change/submission-16.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    private readonly Dictionary<int, int> _cache = new();
+
+    public int CoinChange(int[] coins, int amount) 
+    {
+        if (amount == 0) return 0;
+        if (_cache.TryGetValue(amount, out var count))
+            return count;
+
+        count = int.MaxValue;
+        foreach (var coin in coins)
+        {
+            if (amount - coin < 0) continue;
+            var prevCount = CoinChange(coins, amount - coin);
+            if (prevCount != -1) count = Math.Min(count, 1 + prevCount);
+        }
+
+        _cache[amount] = count == int.MaxValue ? -1 : count;
+        return _cache[amount];
+    }
+}
