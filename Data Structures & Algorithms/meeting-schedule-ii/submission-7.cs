2586680c8e53f9d6ba9b2ba009b7b31027f26629
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-08-13 15:14:51 +0500

    Add: meeting-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-7.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public int MinMeetingRooms(List<Interval> intervals)
+    {
+        intervals.Sort((a, b) => a.start.CompareTo(b.start));
+        var rooms = new PriorityQueue<Interval, int>();
+
+        foreach (var interval in intervals)
+        {
+            if (rooms.Count > 0 && rooms.Peek().end <= interval.start)
+                rooms.Dequeue();
+
+            rooms.Enqueue(interval, interval.end);
+        }
+
+        return rooms.Count;
+    }
+}
