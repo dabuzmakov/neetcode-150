934f5107dabe9fe6264f32b8cf026d7b633af464
dabuzmakov <162581963+dabuzmakov@users.noreply.github.com>
2026-08-15 12:42:24 +0500

    Add: jump-game-ii - submission-7

---
+++ Data Structures & Algorithms/jump-game-ii/submission-7.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int Jump(int[] nums)
+    {
+        var (left, right) = (0, 0);
+        var steps = 0;
+
+        while (right < nums.Length - 1)
+        {
+            var maxStep = 0;
+            for (var i = left; i <= right; i++)
+                maxStep = Math.Max(i + nums[i], maxStep);
+
+            left = right + 1;
+            right = maxStep;
+            steps++;
+        }
+
+        return steps;
+    }
+}
